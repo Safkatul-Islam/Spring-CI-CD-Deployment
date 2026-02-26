9d8b721fbf8b4db9fd0c0eb47dd6723e826a8a9a
Md Safkatul Islam <mdsafkatul.stu24@gmail.com>
2026-02-25 16:31:31 -0800

    scaffold of a spring boot project

---
+++ src/test/java/org/deployment/springbootclouddeployment/SpringBootCloudDeploymentTests.java
@@ -0,0 +1,13 @@
+package org.deployment.springbootclouddeployment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCloudDeploymentTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
